4f544c046489f194767c933beee0710d043e4b9e
Ashish-1062 <83691659+Ashish-1062@users.noreply.github.com>
2023-06-21 14:29:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-function convertDecimalToBinary() {
-	const decimalInput = document.getElementById('decimal-number');
-	const resultDiv = document.getElementById('result');
-	const decimalNum = parseInt(decimalInput.value);
+function decimalToBinary(decimalNum) {
+  let binaryNum = '';
 
-	// Function to convert decimal to binary
-	function decimalToBinary(decimalNum) {
-		let binaryNum = '';
+  // Handling the case when the input is 0
+  if (decimalNum === 0) {
+    binaryNum = '0';
+  }
 
-		// Handling the case when the input is 0
-		if (decimalNum === 0) {
-			binaryNum = '0';
-		}
+  // Converting decimal to binary
+  while (decimalNum > 0) {
+    binaryNum = (decimalNum % 2) + binaryNum;
+    decimalNum = Math.floor(decimalNum / 2);
+  }
 
-		// Converting decimal to binary
-		while (decimalNum > 0) {
-			binaryNum = (decimalNum % 2) + binaryNum;
-			decimalNum = Math.floor(decimalNum / 2);
-		}
-
-		return binaryNum;
-	}
-
-	// Convert the decimal number to binary
-	const binaryNum = decimalToBinary(decimalNum);
-
-	// Display the result
-	resultDiv.textContent = `Binary: ${binaryNum}`;
+  return binaryNum;
 }
+
+module.exports = decimalToBinary;
